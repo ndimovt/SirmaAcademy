e854a1f615bfd362b42d9e2a2f8223e22896d1a9
ndimovt <nikolaitodorov119@gmail.com>
2024-07-06 14:55:13 +0300

    renamed SaveBookInvo.java to SaveBook.java in dataSaver package

--- solid/srp/dataSaver/SaveBook.java
+++ solid/srp/dataSaver/SaveBook.java
@@ -3,7 +3,7 @@ package io.github.ndimovt.solid.srp.dataSaver;
 import io.github.ndimovt.solid.srp.abstractDataWriter.DatabaseWriting;
 import io.github.ndimovt.solid.srp.entity.Book;
 
-public class SaveBookInvo extends DatabaseWriting {
+public class SaveBook extends DatabaseWriting {
     public void saveBook(Book book){
         //i will not implement it because i don't have specific details
     }
